61dc643d19670eca3eccc7f94cb0822ec9cf6c3f
ari <ari.pras1618@gmail.com>
2017-08-13 00:14:04 +0900

    change config

--- keyboards/lets_split/keymaps/jfawkes/config.h
+++ keyboards/lets_split/keymaps/jfawkes/config.h
@@ -30,9 +30,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define _MASTER_RIGHT
-// #define EE_HANDS
+//#define MASTER_LEFT
+//#define _MASTER_RIGHT
+#define EE_HANDS
 
 
 #define MOUSEKEY_DELAY             80
