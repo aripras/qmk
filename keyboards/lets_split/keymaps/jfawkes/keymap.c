107347e7c3869e5be90741636a1311ab705d076d
ari <ari.pras1618@gmail.com>
2017-08-12 21:55:06 +0900

    update layout

--- keyboards/lets_split/keymaps/jfawkes/keymap.c
+++ keyboards/lets_split/keymaps/jfawkes/keymap.c
@@ -35,7 +35,8 @@ enum {
   TD_CLOSE_CURLY = 3,
   TD_QUOTDOUBLE = 4,
   TD_HYPHEN = 5,
-  TD_BACKSLASH = 6
+  TD_BACKSLASH = 6,
+  TD_ESC_CAPS = 7
 };
 
 //Tap Dance Definitions
@@ -47,7 +48,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_CLOSE_CURLY] = ACTION_TAP_DANCE_DOUBLE(S(KC_RBRC), KC_RBRC),
   [TD_QUOTDOUBLE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, S(KC_QUOT)),
   [TD_HYPHEN] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, S(KC_MINS)),
-  [TD_BACKSLASH] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS)
+  [TD_BACKSLASH] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS),
+  [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -74,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  GUI,  KC_ESC, KC_LALT, KC_LGUI, KC_BSPC,   LOWER,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   GUI \
+  GUI,  TD(TD_ESC_CAPS), KC_LALT, KC_LGUI, KC_BSPC,   LOWER,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   GUI \
 ),
 
 /* Colemak
